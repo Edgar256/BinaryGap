a67829935e3efc0a4d2148d6d3bfea151a1243b6
Edgar Tinkamanyire <tinka.edgar@gmail.com>
2022-08-06 18:59:43 +0300

    Update .js

--- .js
+++ .js
@@ -4,6 +4,7 @@
 module.exports = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
+  //not so useful comment   
 
   // Stop running tests after `n` failures
   // bail: 0,
